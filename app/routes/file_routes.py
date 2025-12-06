920a67c6f3ad5a471470ac0862c7404020709379
Lê Tín Nghĩa <nghialt3670@192.168.2.22>
2025-12-06 21:45:35 +0700

    fix: update routes

--- app/routes/file_routes.py
+++ app/routes/file_routes.py
@@ -10,22 +10,47 @@ from app.dependencies.mongo_dependencies import get_bucket
 router = APIRouter(prefix="/files", tags=["files"])
 
 
-@router.post("/upload")
-async def upload(
-    file: UploadFile = File(...), bucket: AsyncIOMotorGridFSBucket = Depends(get_bucket)
+@router.post("")
+async def upload_file(
+    file: UploadFile = File(...),
+    bucket: AsyncIOMotorGridFSBucket = Depends(get_bucket)
 ):
     file_bytes = await file.read()
     file_id = await bucket.upload_from_stream(file.filename, file_bytes)
     return str(file_id)
 
 
-@router.get("/download/{file_id}")
-async def download(
-    file_id: str, bucket: AsyncIOMotorGridFSBucket = Depends(get_bucket)
+@router.get("/{file_id}")
+async def download_file(
+    file_id: str,
+    bucket: AsyncIOMotorGridFSBucket = Depends(get_bucket)
 ):
     grid_out = await bucket.open_download_stream(ObjectId(file_id))
     return StreamingResponse(
         io.BytesIO(await grid_out.read()),
         media_type="application/octet-stream",
-        headers={"Content-Disposition": f'attachment; filename="{grid_out.filename}"'},
+        headers={
+            "Content-Disposition": f'attachment; filename="{grid_out.filename}"'
+        },
     )
+
+
+@router.put("/{file_id}")
+async def replace_file(
+    file_id: str,
+    file: UploadFile = File(...),
+    bucket: AsyncIOMotorGridFSBucket = Depends(get_bucket)
+):
+    await bucket.delete(ObjectId(file_id))
+    file_bytes = await file.read()
+    await bucket.upload_from_stream_with_id(ObjectId(file_id), file.filename, file_bytes)
+    return file_id
+
+
+@router.delete("/{file_id}")
+async def delete_file(
+    file_id: str,
+    bucket: AsyncIOMotorGridFSBucket = Depends(get_bucket)
+):
+    await bucket.delete(ObjectId(file_id))
+    return file_id
